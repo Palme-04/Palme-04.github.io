2a2038db5f2e617d2bc49443987b9121ef4b5a47
Palmetzhofer <martin.palme04@gmail.com>
2024-08-25 17:11:25 +0200

    Finnished the basic function of the page

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const canvas = document.querySelector("#canvas");
 const red = document.querySelector('#red');
 const blue = document.querySelector('#blue');
-let color = 'yellow';
-drawCanvas(4);
+const left = document.querySelector("#left_flex_container")
+let color = 'black';
+let alert = false;
+drawCanvas(16);
 const btn = document.querySelector("#btn");
 
 canvas.addEventListener('mouseover', function(event){
@@ -19,20 +21,46 @@ btn.addEventListener('click', function updateCanvas(){
     drawCanvas(canvasSize);
 })
 
+black.addEventListener('click', () =>{
+    color = 'black';
+})
+
 red.addEventListener('click', () =>{
     color = 'red';
 })
 
+blue.addEventListener('click', () =>{
+    color = 'blue';
+})
+
+eraser.addEventListener('click', () =>{
+    color = 'white';
+})
 
 function drawCanvas(size){
-    for (let i = 0; i < size; i++){
-        const row = document.createElement('div');
-        row.classList.add('row');
-        for (let j = 0; j < size; j++){
+    if(size < 0 || size > 100){
+        if(!alert){
             const div = document.createElement('div');
-            div.classList.add('pixel');
-            row.appendChild(div);
+            div.classList.add("alert");
+            div.textContent = "Only numbers between 0 and 100!"
+            left.appendChild(div);
+            alert = true;
         }
-        canvas.appendChild(row);
+       
+    }else{
+        if (alert){
+            document.querySelector(".alert").remove();
+        }
+        for (let i = 0; i < size; i++){
+            const row = document.createElement('div');
+            row.classList.add('row');
+            for (let j = 0; j < size; j++){
+                const div = document.createElement('div');
+                div.classList.add('pixel');
+                row.appendChild(div);
+            }
+            canvas.appendChild(row);
+        }
     }
+    
 }
